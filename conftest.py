5b79cdedf60341397750c84d031afe8e16a7749d
zagoranov <pijammer@gmail.com>
2020-01-14 22:27:09 +0300

    more tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from bingos import *
 
+
 @pytest.fixture
-def generate_player():
+def generate_npc():
     npc = NPC("Arthur")
     npc.set_card(npc.get_card())
     return npc
+
+
+def generate_human(generate_npc):
+    player = Human('John')
+    player.set_card(generate_npc.get_card())
+    player.hand.append(player.card)
+    return player
+
--- pytest/test_lotto.py
+++ pytest/test_lotto.py
@@ -1,5 +1,17 @@
-def test_generate_player(generate_player):
-	assert generate_player.name == "Arthur"
-	assert len(generate_player.card) == 5
-	assert len(generate_player.deq) == 89
-	assert generate_player.toss_a_number() > 9
+from bingos import *
+
+
+def test_generate_npc(generate_npc):
+    assert generate_npc.name == "Arthur"
+    assert len(generate_npc.card) == 5
+    assert len(generate_npc.deq) == 89
+    assert generate_npc.toss_a_number() > 9
+
+
+def test_winning(generate_npc, generate_human):
+    assert generate_npc.check_hand(generate_human)
+
+
+def test_loosing(generate_npc, generate_human):
+    generate_human.hand.clear()
+    assert generate_npc.check_hand(generate_human) == False
